759194160e467d4d3fa2db6bcc42529e281b2dde
BrotherLi <brotherli.2026@gmail.com>
2026-06-13 13:31:36 +0300

    Fix robots sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://sleep-project.vercel.app/sitemap.xml",
+    sitemap: "https://sleep-project-khaki.vercel.app/sitemap.xml",
   };
 }
